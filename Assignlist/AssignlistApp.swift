47509b712cbd4751d1bb9bfaa992d77a537db4fb
dezzw <dw@dezzw.com>
2023-07-29 19:17:48 +0800

    Initial Commit

---
+++ Assignlist/AssignlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignlistApp.swift
+//  Assignlist
+//
+//  Created by Desmond Wang on 2023-07-29.
+//
+
+import SwiftUI
+
+@main
+struct AssignlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
